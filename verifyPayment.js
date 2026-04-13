fcb75466d6767ffb239100a5a570da827e777509
dcx2024 <166218735+dcx2024@users.noreply.github.com>
2026-04-13 10:17:07 +0100

    Add item name and amount to WhatsApp message

--- verifyPayment.js
+++ verifyPayment.js
@@ -44,6 +44,7 @@ app.post('/verify', async(req, res) => {
 
         const { event, data } = req.body;
         const customerPhone = data.metadata?.whatsapp_number;
+        const itemName=data.metadata?.item_name
         const reference=data.reference;
         // 3. Process events
         switch (event) {
